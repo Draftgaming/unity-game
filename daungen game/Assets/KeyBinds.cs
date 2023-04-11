e1d1b49d1632377d913864dfceab1d1e86b32988
Draftgaming <130181218+Draftgaming@users.noreply.github.com>
2023-04-11 22:24:08 +0300

    started map

---
+++ daungen game/Assets/KeyBinds.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 80.0f; // Code for how fast the ball can move. Also it will be public so we can change it inside of Unity itself. 
+    public Rigidbody Player; //Player 1 Rigidbody
+   
+
+
+    //Player 1 Code with aswd keys
+    void Player1Movement()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            Player.AddForce(Vector3.left * speed);
+
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            Player.AddForce(Vector3.right * speed);
+
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            Player.AddForce(Vector3.forward * speed);
+
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            Player.AddForce(Vector3.back * speed);
+
+        }
+    }
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        Player1Movement();
+       
+    }
+}
